b4c6191a2c35ff695eb68d0253281ab1cdcf1d11
Antonioraa128 <antonio.rivera@udem.edu>
2025-12-03 21:20:42 -0600

    Optimizado, sin explicacion

--- Alvarez_Rivera_EP3.py
+++ Alvarez_Rivera_EP3.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression # Regresión lineal
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score # Errores
@@ -12,13 +13,27 @@ st.set_page_config(page_title="Proyecto Final IA", layout="wide")
 
 # Leemos el dataset
 df = pd.read_csv('computer_prices_all.csv')
-df = df.select_dtypes(include=np.number) #Solo numericas
 
 #Limpia de datos y mapeo
-#[Pendiente]
+#Mapeo de la variable device type, se vuelve binario si es laptop o no
+mapeo_type = {'Desktop': 0, 'Laptop': 1}
+df['device_type'] = df['device_type'].map(mapeo_type)
+df.head()
+
+#Dummies de las variables categoricas para hacer columnas binarias
+df = pd.get_dummies(df, columns=['display_type'], prefix=['DT'], dtype=int)
+df.drop(columns=['DT_VA'], inplace=True) #Se quita para evitar redundancia
+df.head()
+
+#Dummies de las variables categoricas para hacer columnas binarias
+df = pd.get_dummies(df, columns=['gpu_brand'], prefix=['GPU'], dtype=int)
+df.drop(columns=['GPU_Intel'], inplace=True) #Se quita para evitar redundancia
+df.head()
+
+df = df.select_dtypes(include=np.number) #Dejamos solo las variables numericas
 
 # Entrenamiento del modelo: Se entrena en vivo para el demo
-X = df[['gpu_tier', 'cpu_tier', 'ram_gb']]
+X = df[['cpu_cores','cpu_base_ghz', 'gpu_tier', 'GPU_Apple', 'GPU_NVIDIA', 'GPU_AMD', 'ram_gb', 'device_type', 'display_size_in',  'release_year', 'storage_gb', 'refresh_hz','DT_LED','DT_OLED' ,'DT_IPS', 'DT_QLED', 'DT_Mini-LED']]
 y = df['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -61,24 +76,11 @@ if opcion == "1. Contexto y Problema":
 elif opcion == "2. Análisis Exploratorio de Datos (EDA)":
     st.title("📊 Análisis Exploratorio de Datos")
     
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        st.subheader("Relación gpu_tier vs Precio")
-        st.scatter_chart(data=df, x='gpu_tier', y='price', color="#3E0080") 
-        st.caption("Correlación positiva: AAAAAAAAAAAAAAAAAAAA")
-        
-    '''with col2:
-        st.subheader("Distribución por cpu_tier")
-        conteo_zona = df['cpu_tier'].value_counts().rename(index={0: 'Periferia', 1: 'Centro'})
-        st.bar_chart(conteo_zona, color="#FF8C0095")
-        st.caption("Distribución de propiedades en el dataset.")
-    '''
-
-    with col2:
-        st.subheader("Relación cpu_tier vs Precio")
-        st.scatter_chart(data=df, x='cpu_tier', y='price', color="#3E0080") 
-        st.caption("Correlación positiva: AAAAAAAAAAAAAAAAAAAA")
+    corr = df.corr()
+    st.write("Matriz de correlación (Heatmap):")
+    fig, ax = plt.subplots(figsize=(16, 12))
+    sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm")
+    st.pyplot(fig)
 
 # ------------------------------------------------------------------
 # Sección 3. Evaluación del modelo
@@ -94,9 +96,7 @@ elif opcion == "3. Evaluación del Modelo":
     col3.metric("Test Set Size", len(y_test))
     
     st.subheader("Gráfica: Realidad vs. Predicción")
-    # Alinear predicciones con el índice de y_test y ordenar para evitar saltos en la línea
-    preds_series = pd.Series(preds, index=y_test.index)
-    comparison_df = pd.DataFrame({'Real': y_test, 'Predicho': preds_series}).sort_index()
+    comparison_df = pd.DataFrame({'Real': y_test, 'Predicho': preds}).sort_index()
     # Colores de alto contraste: Rojo (Real) y Azul (Predicho)
     st.line_chart(comparison_df.head(30), color=["#FF0000", "#0000FF"], use_container_width=True)
 
@@ -104,37 +104,76 @@ elif opcion == "3. Evaluación del Modelo":
 # Sección 4. Predicción en vivo
 elif opcion == "4. Predicción en Vivo":
     st.title("🚀 Prueba del Modelo en Tiempo Real")
-    st.markdown("Configure las características de la casa para obtener una valuación instantánea.")
+    st.markdown("Configure las características de la computadora para obtener una valuación instantánea.")
     
     # Inputs interactivos para el profesor/usuario
-    col1, col2 = st.columns(2)
-    with col1:
-        cpu_input = st.slider("Cpu tier", 1, 6, 2)
-        ram_input = st.number_input("Gp de ram", 4, 64, 16)
+    col1, col2, col3, col4 = st.columns(4)
 
+    #Cosas del CPU
+    with col1:
+        st.subheader("Caracteristicas del CPU")
+        cpu_cores = st.slider("CPU cores", 4, 26, 8)
+        cpu_ghz = st.number_input("CPU GHZ base", 2.0, 3.4, 2.6)
+
+    #Cosas del GPU
     with col2:
-        gpu_input = st.slider("Gpu tier", 1, 6, 2)
+        st.subheader("Caracteristicas del GPU")
+        gpu_input = st.slider("GPU tier", 1, 6, 3)
+        gpu_brand_input = st.selectbox("Marca del GPU", ["Apple", "NVIDIA", "AMD", "Intel"])
+        apple_binaria = 1 if gpu_brand_input == "Apple" else 0
+        nvidia_binaria = 1 if gpu_brand_input == "NVIDIA" else 0
+        amd_binaria = 1 if gpu_brand_input == "AMD" else 0
+
     
-    '''
-    with col2:
-        antiguedad_input = st.slider("Antigüedad (Años)", 0, 50, 5)
-        zona_input = st.selectbox("Ubicación", ["Periferia", "Centro"])
-        zona_binaria = 1 if zona_input == "Centro" else 0
-    '''
+    #Otras cosas
+    with col3:
+        st.subheader("Caracteristicas del Monitor")
+        size_input = st.number_input("Tamaño del display", 13.3, 34.0, 16.0)
+        hz_input = st.slider("Hercios por segundo", 60, 240, 120)
+        display_type_input = st.selectbox("Tipo de display", ["LED", "Mini-LED", "OLED", "QLED", "IPS", "VA"])
+        led_binaria = 1 if display_type_input == "LED" else 0
+        mled_binaria = 1 if display_type_input == "Mini-LED" else 0
+        oled_binaria = 1 if display_type_input == "OLED" else 0
+        qled_binaria = 1 if display_type_input == "QLED" else 0
+        ips_binaria = 1 if display_type_input == "IPS" else 0
+
+    with col4:
+        st.subheader("Otras caracteristicas")
+        device_input = st.selectbox("Dispositivo", ["Laptop", "Desktop"])
+        year_input = st.slider("Año del dispositivo", 2018, 2025, 2022)
+        device_binaria = 1 if device_input == "Laptop" else 0
+        ram_input = st.slider("Gb de ram", 8, 144, 32)
+        storage_input = st.slider("Almacenamiento", 256, 4096, 512)
+
+
 
     # Botón de predicción
     if st.button("Calcular Precio Justo", type="primary"):
         input_data = pd.DataFrame({
+            'cpu_cores': [cpu_cores],
+            'cpu_base_ghz': [cpu_ghz],
             'gpu_tier': [gpu_input],
-            'cpu_tier': [cpu_input],
+            'GPU_Apple': [apple_binaria],
+            'GPU_NVIDIA': [nvidia_binaria],
+            'GPU_AMD': [amd_binaria],
             'ram_gb': [ram_input],
+            'device_type': [device_binaria],
+            'display_size_in': [size_input],
+            'release_year': [year_input],
+            'storage_gb': [storage_input],
+            'refresh_hz': [hz_input],
+            'DT_LED': [led_binaria],
+            'DT_OLED': [oled_binaria],
+            'DT_IPS': [ips_binaria],
+            'DT_QLED': [qled_binaria],
+            'DT_Mini-LED': [mled_binaria]
         })
         
         input_data = scaler.transform(input_data) #la escalamos antes de meterla al modelo
         prediccion = model.predict(input_data)[0]
         
         # Detalle visual del resultado
-        st.success(f"💰 Precio Estimado: **${prediccion:,.2f} MXN**")
+        st.success(f"💰 Precio Estimado: **${prediccion:,.2f} USD**")
         
         max_price = df['price'].max()
         st.progress(int(min(prediccion / max_price, 1.0) * 100))
